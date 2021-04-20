2ba68138a8eca5293de3bf5762efde255af615c9
Gourav Mohanty <gouravmohanty2001@gmail.com>
2021-04-20 21:17:57 +0530

    added data (#26)
    added profile data in teamdata.jsx
    
    Co-authored-by: Aniket Pal <aniketindian8@gmail.com>

--- src/pages/teamData.jsx
+++ src/pages/teamData.jsx
@@ -17,12 +17,22 @@ const teamData = [
             "instagram":"https://www.instagram.com/sumanasuchira"  
     },
     {
+
+        "name":"Gourav Mohanty",
+        "image":"https://instagram.fbbi1-2.fna.fbcdn.net/v/t51.2885-15/e35/134084568_1089937848095660_2794529853113019120_n.jpg?tp=1&_nc_ht=instagram.fbbi1-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=1gkfQBNBqR8AX-t0Kvo&edm=AP_V10EAAAAA&ccb=7-4&oh=515c0aa166ef0d85edf26a3f541f1dbb&oe=60A534E1&_nc_sid=4f375e",
+        "tagline":"Member",
+        "github":"https://github.com/gouravmohanty7070",
+        "linkedin":"https://www.linkedin.com/in/gourav-mohanty-78006a201",
+        "instagram":"https://www.instagram.com/_gourav.mohanty/"  
+    },
+  {
         "name":"Sneha Mallik",
         "image":require('../assets/data/sneha.jpg').default,
         "tagline":"President",
         "github":"https://github.com/snehaM2210",
         "linkedin":"https://www.linkedin.com/in/sneha-mallik-736170191/",
         "instagram":"https://www.instagram.com/_sneham2210/"  
+
     }
 ]
 
