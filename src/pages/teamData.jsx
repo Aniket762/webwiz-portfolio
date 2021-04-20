86fe04a6d14628975a6834d66eaf5cb7ed98362f
gouravmohanty7070 <77116228+gouravmohanty7070@users.noreply.github.com>
2021-04-20 20:38:33 +0530

    added data
    added profile data in teamdata.jsx

--- src/pages/teamData.jsx
+++ src/pages/teamData.jsx
@@ -24,6 +24,14 @@ const teamData = [
             "github":"https://www.github.com/sumana2001",
             "linkedin":"https://www.linkedin.com/in/sumana-basu/",
             "instagram":"https://www.instagram.com/sumanasuchira"  
+    },
+    {
+        "name":"Gourav Mohanty",
+        "image":"https://instagram.fbbi1-2.fna.fbcdn.net/v/t51.2885-15/e35/134084568_1089937848095660_2794529853113019120_n.jpg?tp=1&_nc_ht=instagram.fbbi1-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=1gkfQBNBqR8AX-t0Kvo&edm=AP_V10EAAAAA&ccb=7-4&oh=515c0aa166ef0d85edf26a3f541f1dbb&oe=60A534E1&_nc_sid=4f375e",
+        "tagline":"Member",
+        "github":"https://github.com/gouravmohanty7070",
+        "linkedin":"https://www.linkedin.com/in/gourav-mohanty-78006a201",
+        "instagram":"https://www.instagram.com/_gourav.mohanty/"  
     }
 ]
 
