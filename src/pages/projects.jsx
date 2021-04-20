425230b349bdf829f15b602bc580904e006a1843
Aniket762 <aniketindian8@gmail.com>
2021-04-20 20:21:32 +0530

    minor

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from 'react';
+import Card from '../components/card/card';
+import projectData from './projectsData';
+import {Grid} from '@material-ui/core';
+import styled from "styled-components";
 
-function projects() {
-    return (
-        <div className='App' >
-            <h1>PROJECTS</h1>
-        </div>
-    )
-}
+
+const SubHeading = styled.div`
+  align-self:flex-end;
+  font-size: 40px;
+  color: #710193;
+
+  font-weight:600;
+
+  @media screen and (max-width: 1324px) {  
+    font-size:40px;
+  }
  
-export default projects
+`;
+
+const Content = styled.p`
+  font-family: "metropolis-regular", sans-serif;
+  font-size: 25px;
+  color: rgba(85, 85, 85, 0.95);
+
+  @media screen and (max-width: 1324px) {  
+    font-size:20px;
+  }
+ 
+`;
+
+function createCard(projectData) {
+    return (
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+      <Card
+        key={projectData.id}
+        image={projectData.image}
+        name={projectData.name}
+        tagline={projectData.tagline}
+        github={projectData.github}
+        linkedin={projectData.linkedin}
+        instagram = {projectData.instagram}    
+      />
+      </Grid>
+    );
+  }
+
+function team() {
+    return (
+        <div className='App'>
+            <SubHeading> #Projects</SubHeading>
+            <Content> Every student has strategic goals and the projects that we do for them advance those goals</Content>
+           
+            <Grid lg={12} item container spacing={2}  align='center'>
+            {projectData.map(createCard)}
+            </Grid>
+     
+            
+                        
+        </div>
+    );
+}
+
+export default team
--- src/pages/projectsData.jsx
+++ src/pages/projectsData.jsx
@@ -24,7 +24,7 @@ const projectData = [
         "image":"https://media.istockphoto.com/photos/yellow-origami-paper-boat-on-blue-background-picture-id1150244288?k=6&m=1150244288&s=170667a&w=0&h=BdjJf59PxjKFc4pe1KAqkJDXHpewTFgD8Vra1NAoSqg=",
         "tagline":"lorem ipsum blah blah",
         "github":"https://www.github.com",
-}
+    }
 ]
 
 export default projectData;
