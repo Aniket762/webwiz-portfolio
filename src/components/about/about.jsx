b88f0388ced7044a29854701e5f5dfc9fe8d8695
sumana2001 <sumana.basu2001@gmail.com>
2021-04-20 16:19:05 +0530

    About heading

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import styled from "styled-components";
 
-const Heading = styled.div`
-  align-self:flex-end;
-
-  font-size: 80px;
-  line-height:0;
+const Heading = styled.h1`
+  text-align:center;
+  font-size: 60px;
   letter-spacing: 0;
   color: #000000;
   letter-spacing: -0.03rem;
-  margin-top: 0;
-  margin-bottom: 4.8rem;
-
-  @media screen and (max-width: 762px) {  
-    font-size:25px;
-  }
- @media screen and (min-width:762px && max-width: 1324px) {  
-    font-size:25px;
+  margin-top: -20px;
+  margin-bottom:-5px;
+  @media screen and (max-width:600px){
+    font-size:40px;
   }
  
 `;
@@ -34,9 +28,9 @@ const SubHeading = styled.div`
  
 `;
 
-const Content = styled.div`
+const Content = styled.p`
   font-family: "metropolis-regular", sans-serif;
-  font-size: 40px;
+  font-size: 30px;
   padding:50px;
   margin-top:-40px;
   line-height: 1.7;
@@ -55,6 +49,11 @@ function about() {
         <SubHeading>#About Us</SubHeading>
         <Heading>Not just any other technical club</Heading>
         <Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati ten.etur iure eius earum ut molestias architecto voluptate
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
 molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
 numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
 optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
