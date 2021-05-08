1990d2c4a20cc71ad40b7f574b9d3d83d87cd7a1
Hemant Bajaj <41hemu38@gmail.com>
2021-05-09 00:29:10 +0530

    hemant data

--- src/pages/teamData.jsx
+++ src/pages/teamData.jsx
@@ -1,78 +1,78 @@
-
 const teamData = [
-    {
-            "name":"Aniket Pal",
-            "image":require('../assets/data/aniket.jpeg').default,
-            "tagline":"Founder",
-            "github":"https://www.github.com/Aniket762",
-            "linkedin":"https://www.linkedin.com/in/aniket-pal/",
-            "instagram":"https://www.instagram.com/itzaniket_762"
-        
-    },
-    
   {
-        "name":"Sneha Mallik",
-        "image":require('../assets/data/sneha.jpg').default,
-        "tagline":"President",
-        "github":"https://github.com/snehaM2210",
-        "linkedin":"https://www.linkedin.com/in/sneha-mallik-736170191/",
-        "instagram":"https://www.instagram.com/_sneham2210/"  
+    name: "Aniket Pal",
+    image: require("../assets/data/aniket.jpeg").default,
+    tagline: "Founder",
+    github: "https://www.github.com/Aniket762",
+    linkedin: "https://www.linkedin.com/in/aniket-pal/",
+    instagram: "https://www.instagram.com/itzaniket_762",
+  },
 
-    },
-    {
-            "name":"Sumana Basu",
-            "image":require('../assets/data/sumana.jpeg').default,
-            "tagline":"Ex-Coordinator",
-            "github":"https://www.github.com/sumana2001",
-            "linkedin":"https://www.linkedin.com/in/sumana-basu/",
-            "instagram":"https://www.instagram.com/sumanasuchira"  
-    },
-    {
-
-
-        "name":"Sanjeet Patro",
-        "image":require('../assets/data/farewell.jpg').default,
-        "tagline":"Design Team",
-        "github":"https://github.com/sanjeet-crypto",
-        "linkedin":"https://www.linkedin.com/in/sanjeet-patro-6a1067202/",
-        "instagram":"https://www.instagram.com/sanjeet_patro/"  
-    },
-    {
-
-        "name":"Sayantan Chatterjee",
-        "tagline":"Mangement Team",
-        "image": require('../assets/data/SayantanChatterjee.jpeg').default ,
-        "github":"https://github.com/Sayantan-Ch",
-        "linkedin":"https://www.linkedin.com/in/sayantan-chatterjee-400784208/",
-        "instagram":"https://www.instagram.com/sayantan__ch/"  
-    },
   {
-        "name":"Gourav Mohanty",
-        "image":"https://instagram.fbbi1-2.fna.fbcdn.net/v/t51.2885-15/e35/134084568_1089937848095660_2794529853113019120_n.jpg?tp=1&_nc_ht=instagram.fbbi1-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=1gkfQBNBqR8AX-t0Kvo&edm=AP_V10EAAAAA&ccb=7-4&oh=515c0aa166ef0d85edf26a3f541f1dbb&oe=60A534E1&_nc_sid=4f375e",
-        "tagline":"Mangement Team",
-        "github":"https://github.com/gouravmohanty7070",
-        "linkedin":"https://www.linkedin.com/in/gourav-mohanty-78006a201",
-        "instagram":"https://www.instagram.com/_gourav.mohanty/"  
-    },
-    {
-            "name":"Ankush Kumar Lohani",
-            "image":require('../assets/data/ankush.jpeg').default,
-            "tagline":"Management Team",
-            "github":"https://github.com/iamakl",
-            "linkedin":"https://www.linkedin.com/in/iamakl/",
-            "instagram":"https://www.instagram.com/iam__akl/"
-        
-    },
-    {
-            "name":"Komal",
-            "image":require('../assets/data/komald.jpeg').default,
-            "tagline":"Web Developer",
-            "github":"https://github.com/komal1503",
-            "linkedin":"https://www.linkedin.com/in/komal-deegwal-35b31a206",
-            "instagram":"https://www.instagram.com/komal15012003/"
-        
-    }
-
-]
+    name: "Sneha Mallik",
+    image: require("../assets/data/sneha.jpg").default,
+    tagline: "President",
+    github: "https://github.com/snehaM2210",
+    linkedin: "https://www.linkedin.com/in/sneha-mallik-736170191/",
+    instagram: "https://www.instagram.com/_sneham2210/",
+  },
+  {
+    name: "Sumana Basu",
+    image: require("../assets/data/sumana.jpeg").default,
+    tagline: "Ex-Coordinator",
+    github: "https://www.github.com/sumana2001",
+    linkedin: "https://www.linkedin.com/in/sumana-basu/",
+    instagram: "https://www.instagram.com/sumanasuchira",
+  },
+  {
+    name: "Sanjeet Patro",
+    image: require("../assets/data/farewell.jpg").default,
+    tagline: "Design Team",
+    github: "https://github.com/sanjeet-crypto",
+    linkedin: "https://www.linkedin.com/in/sanjeet-patro-6a1067202/",
+    instagram: "https://www.instagram.com/sanjeet_patro/",
+  },
+  {
+    name: "Sayantan Chatterjee",
+    tagline: "Mangement Team",
+    image: require("../assets/data/SayantanChatterjee.jpeg").default,
+    github: "https://github.com/Sayantan-Ch",
+    linkedin: "https://www.linkedin.com/in/sayantan-chatterjee-400784208/",
+    instagram: "https://www.instagram.com/sayantan__ch/",
+  },
+  {
+    name: "Gourav Mohanty",
+    image:
+      "https://instagram.fbbi1-2.fna.fbcdn.net/v/t51.2885-15/e35/134084568_1089937848095660_2794529853113019120_n.jpg?tp=1&_nc_ht=instagram.fbbi1-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=1gkfQBNBqR8AX-t0Kvo&edm=AP_V10EAAAAA&ccb=7-4&oh=515c0aa166ef0d85edf26a3f541f1dbb&oe=60A534E1&_nc_sid=4f375e",
+    tagline: "Mangement Team",
+    github: "https://github.com/gouravmohanty7070",
+    linkedin: "https://www.linkedin.com/in/gourav-mohanty-78006a201",
+    instagram: "https://www.instagram.com/_gourav.mohanty/",
+  },
+  {
+    name: "Ankush Kumar Lohani",
+    image: require("../assets/data/ankush.jpeg").default,
+    tagline: "Management Team",
+    github: "https://github.com/iamakl",
+    linkedin: "https://www.linkedin.com/in/iamakl/",
+    instagram: "https://www.instagram.com/iam__akl/",
+  },
+  {
+    name: "Komal",
+    image: require("../assets/data/komald.jpeg").default,
+    tagline: "Web Developer",
+    github: "https://github.com/komal1503",
+    linkedin: "https://www.linkedin.com/in/komal-deegwal-35b31a206",
+    instagram: "https://www.instagram.com/komal15012003/",
+  },
+  {
+    name: "Hemant Bajaj",
+    image: require("../assets/data/hemant.jpg").default,
+    tagline: "Web Developer",
+    github: "https://github.com/ORKO06",
+    linkedin: "https://www.linkedin.com/in/hemant-bajaj-7b8082204/",
+    instagram: "https://www.instagram.com/hemant_bajaj_/",
+  },
+];
 
 export default teamData;
