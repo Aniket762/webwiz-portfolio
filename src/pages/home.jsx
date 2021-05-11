bc7f807589824c2858728553c377ab51cfd44826
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-05-11 16:36:30 +0200

    fix hero font size on mobile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import Hero from '../components/hero/hero';
-import About from '../components/about/about';
-import Event from '../components/events/events';
-import Team from '../components/team/team';
-import Projects from '../components/projects/projects';
+import React from "react";
+import Hero from "../components/hero/hero";
+import About from "../components/about/about";
+import Event from "../components/events/events";
+import Team from "../components/team/team";
+import Projects from "../components/projects/projects";
 
 function home() {
-    return (
-        <div className="App">
-            <Hero />
-            <About />
-            <Event />
-            <Team />
-            <img style={{width:'100%',marginBottom:-10}} src='https://echoofindia.com/SysFiles/images/banner_india.jpeg' alt="" />
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+      <Event />
+      <Team />
+      <img
+        style={{ width: "100%", marginBottom: -10 }}
+        src="https://echoofindia.com/SysFiles/images/banner_india.jpeg"
+        alt=""
+      />
 
-            <Projects />
-           
-        </div>
-    )
+      <Projects />
+    </div>
+  );
 }
 
-export default home
+export default home;
