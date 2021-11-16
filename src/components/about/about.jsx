c80f52507b0c8437dfd008d085097a7d9f8ed102
Emiliano <russo.emiliano97@gmail.com>
2021-11-16 16:28:23 -0300

    About Us section Rework

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,65 +1,70 @@
-import React from 'react'
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import './about.css';
 
 const Heading = styled.h1`
-  text-align:center;
+  text-align: center;
   font-size: 60px;
   letter-spacing: 0;
   color: #000000;
   letter-spacing: -0.03rem;
-  margin-top: -20px;
-  margin-bottom:-5px;
-  @media screen and (max-width:600px){
-    font-size:40px;
+  margin-bottom: -5px;
+  @media screen and (max-width: 600px) {
+    font-size: 40px;
   }
- 
 `;
 
-const SubHeading = styled.div`
-  align-self:flex-end;
-  font-size: 40px;
-  color: #710193;
-  line-height:4;
-  font-weight:600;
-  margin-top: 50px;
-
-  @media screen and (max-width: 1324px) {  
-    font-size:40px;
-  }
- 
-`;
-
-const Content = styled.p`
-  font-family: "metropolis-regular", sans-serif;
-  font-size: 29px;
-  padding: 3rem 9rem 3rem 9rem;
-  margin-top:-40px;
-  line-height: 1.5;
-  margin-bottom: 3.6rem;
-  color: rgba(85, 85, 85, 0.95);
-  text-align: left;
-
-  @media screen and (max-width: 1324px) {  
-    font-size:20px;
-    padding: 3rem 5rem 3rem 5rem;
-  }
-
-  @media screen and (max-width: 550px) {  
-    font-size:20px;
-    padding: 2rem 3rem 2rem 3rem;
-  }
- 
-`;
+const LiteCard = props => {
+  return (
+    <div
+      className="liteCard"
+      style={{
+        backgroundImage: `url("${props.imageUrl}")`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        color: props.color,
+      }}
+    >
+      {/*Icon*/}
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </div>
+  );
+};
 
 function about() {
-    return (
-        <div>
-        <SubHeading>#About Us</SubHeading>
-        <Heading>Not just any other technical club</Heading>
-        <Content>Webwiz is committed to providing resources and content to the web development community. Our entire focus is on web development and design. #1 club in web development running under Student activity center (SAC) NIT Rourkela. Students from all undergraduate programs with interest in growing as a web developer are warmly welcome. We believe that web development is a -domain that can make a newbie crazy about software development. It doesn’t matter if you are a frontend, full-stack, or backend developer or never even touched a code editor in your life. Webwiz club hopes to provide you with knowledge, roadmaps, and guidance. Of course, our content will focus on and use examples from web-based languages such as Javascript and Python. We are big fans of the React and Node.js framework. Still, we believe you shouldn’t limit yourself in this continually evolving sector of technology. We hope you find something useful!
-        </Content>
+  return (
+    <div id="about">
+      <h1>#About Us</h1>
+      <div className="underline"></div>
+      <Heading>Not just any other technical club</Heading>
+      <div id="cards">
+        <LiteCard
+          title="Made for students"
+          description="Students from all undergraduate programs with interest in growing as a web developer are warmly welcome.  We believe that web development is a -domain that can make a newbie crazy about software development. It doesn’t matter if you are a frontend, full-stack, or backend developer or never even touched a code editor in your life"
+          imageUrl={
+            'https://cdn.pixabay.com/photo/2016/12/29/18/44/background-1939128_960_720.jpg'
+          }
+          color="white"
+        />
+        <LiteCard
+          title="Design Focus"
+          description="Committed to providing resources and content to the web development community. Our entire focus is on web development and design."
+          imageUrl={
+            'https://cdn.pixabay.com/photo/2020/04/08/16/32/keyboard-5017973_960_720.jpg'
+          }
+        />
+        <LiteCard
+          title="Practical Guide"
+          description="Of course, our content will focus on and use examples from web-based languages such as Javascript and Python. We are big fans of the React and Node.js framework. Still, we believe you shouldn’t limit yourself in this continually evolving sector of technology. We hope you find something useful!"
+          imageUrl={
+            'https://cdn.pixabay.com/photo/2018/12/18/22/29/background-3883181_960_720.jpg'
+          }
+          color="white"
+        />
       </div>
-    )
+    </div>
+  );
 }
 
-export default about
+export default about;
