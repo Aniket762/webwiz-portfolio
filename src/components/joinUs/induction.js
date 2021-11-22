d1c97f1548241b0890194bd5e9def4de006bff62
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-22 13:58:56 +0530

    induction sections created

--- src/components/joinUs/induction.js
+++ src/components/joinUs/induction.js
@@ -1,9 +1,46 @@
 import React from "react";
+// import reactDom from "react-dom";
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import { Heading } from "../info/info.elements";
+import inductionData from "./inductionData";
+// import { getThemeProps } from "@material-ui/styles";
+
+
+const useStyles = makeStyles({})
+
+const InductionCard = (props) =>{
+
+    const classes = useStyles();
+    return(
+
+    <div className={classes.inductioncard}>
+        <div className={classes.inductioncardleft}>
+            <Heading>{props.heading}</Heading>
+            <Heading>{props.subheading}</Heading>
+        </div>
+        <div className={classes.inductioncardright}></div>
+    </div>
+    )
+}
 
 function Induction () {
+
+    const classes = useStyles();
     return(
         <>
-        INDUCTION
+         <div className={classes.induction}>
+            <div className={classes.inductioncards}>
+                {inductionData.map(function ncards(val){
+                    return(
+                        <InductionCard 
+                            heading={val.heading}
+                            subheading={val.subheading}
+                        />
+                    )
+                })}
+            </div>
+         </div>
         </>
     )
 }
---
+++ src/components/joinUs/inductionData.js
@@ -0,0 +1,21 @@
+const inductionData =[
+    {
+        id:1,
+        heading: "Technical team",
+        subheading: "Webwiz is committed to providing resources and content to the web development community. Our entire focus is on web development and design",
+    },
+
+    {
+        id:2,
+        heading: "Technical team",
+        subheading: "Webwiz is committed to providing resources and content to the web development community. Our entire focus is on web development and design",
+    },
+
+    {
+        id:3,
+        heading: "Technical team",
+        subheading: "Webwiz is committed to providing resources and content to the web development community. Our entire focus is on web development and design",
+    }
+];
+
+export default inductionData;
