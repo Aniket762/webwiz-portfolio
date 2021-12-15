39b633919430842eaabed3f3f94f7e6a1464ad58
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-16 02:46:16 +0530

    feat: adding pwa support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +17,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//for adding PWA
+serviceWorkerRegistration.register();
